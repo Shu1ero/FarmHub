9266d6fe831705107ac42eab2f9b8ec390d6c178
Shu1ero <37975860+Shu1ero@users.noreply.github.com>
2023-03-17 17:46:29 +0200

    Added main entites for database

---
+++ FarmHub.Shared/Entities/Reviews.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FarmHub.Shared.Entities
+{
+    public class Reviews : EntityBase
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+    }
+}
